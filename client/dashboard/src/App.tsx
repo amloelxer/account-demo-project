46c2bca759269b13aaa98496a1aa2eca3b91be13
Alex Moller <amloelxer@gmail.com>
2024-07-03 17:18:32 -0400

    add pretty

--- client/dashboard/src/App.tsx
+++ client/dashboard/src/App.tsx
@@ -1,31 +1,22 @@
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Dashboard from "./Dashboard/Dashboard";
+import LandingPage from "./LandingPage";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import './App.css';
-import {
-  BrowserRouter,
-  Route,
-  Routes
-} from "react-router-dom";
-import Dashboard from './Dashboard/Dashboard';
-import LandingPage from './LandingPage';
-import {
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query'
-
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="Dashboard" element={<Dashboard />} />
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="Dashboard" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
     </QueryClientProvider>
   );
 }
-
 
 export default App;
--- client/dashboard/src/Dashboard/Dashboard.tsx
+++ client/dashboard/src/Dashboard/Dashboard.tsx
@@ -1,35 +1,45 @@
-import './Dashboard.css';
-import { JSXElementConstructor, ReactElement, ReactNode, ReactPortal, useEffect, useState } from 'react';
-import axios from 'axios';
-import { useQuery } from '@tanstack/react-query';
+import "./Dashboard.css";
+import {
+  JSXElementConstructor,
+  ReactElement,
+  ReactNode,
+  ReactPortal,
+  useEffect,
+  useState,
+} from "react";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
 
 const submitTransfer = async () => {
-    const payload = {
-        "accountSourceId": "7016b52f-eaeb-467e-9a68-897f03bbba35",
-        "accountDestinationId": "14ee89de-beeb-442f-ad4b-fa780d906e08",
-        "transferAmount": 52.28
-    }
+  const payload = {
+    accountSourceId: "7016b52f-eaeb-467e-9a68-897f03bbba35",
+    accountDestinationId: "14ee89de-beeb-442f-ad4b-fa780d906e08",
+    transferAmount: 52.28,
+  };
 
-    axios.defaults.baseURL = 'http://127.0.0.1:3001'
-    try {
-        await axios.post('/submitTransfer', payload)
-    } catch (err) {
-        console.error(err)
-    }
-}
+  axios.defaults.baseURL = "http://127.0.0.1:3001";
+  try {
+    await axios.post("/submitTransfer", payload);
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 const Dashboard = () => {
-    const  [transfers, setTranfers]  = useState([{name: "fake transfer"}])
+  const [transfers, setTranfers] = useState([{ name: "fake transfer" }]);
 
-    return (
-        <div className="container">
-            <button style={{height: 50, width: 100}} onClick={() => {
-                submitTransfer()
-            }}>
-            {'Submit Transfer'}
-            </button>
-        </div>
-    )
-}
+  return (
+    <div className="container">
+      <button
+        style={{ height: 50, width: 100 }}
+        onClick={() => {
+          submitTransfer();
+        }}
+      >
+        {"Submit Transfer"}
+      </button>
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
