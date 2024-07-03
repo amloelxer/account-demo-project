c2933451efed81dec4c2f498b292abb891d3c15e
Alex Moller <amloelxer@gmail.com>
2024-07-03 17:25:11 -0400

    add some comments to the dashboard

--- client/dashboard/src/Dashboard/Dashboard.tsx
+++ client/dashboard/src/Dashboard/Dashboard.tsx
@@ -1,9 +1,5 @@
 import "./Dashboard.css";
 import {
-  JSXElementConstructor,
-  ReactElement,
-  ReactNode,
-  ReactPortal,
   useEffect,
   useState,
 } from "react";
@@ -11,22 +7,21 @@ import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
 
 const submitTransfer = async () => {
-  const payload = {
-    accountSourceId: "7016b52f-eaeb-467e-9a68-897f03bbba35",
-    accountDestinationId: "14ee89de-beeb-442f-ad4b-fa780d906e08",
-    transferAmount: 52.28,
-  };
-
-  axios.defaults.baseURL = "http://127.0.0.1:3001";
-  try {
-    await axios.post("/submitTransfer", payload);
-  } catch (err) {
-    console.error(err);
-  }
+  // Hit the transfer endpoint
 };
+
+const pollForTransfersAndUpdateIfNeeded = () => {
+  // poll for transfers
+}
 
 const Dashboard = () => {
   const [transfers, setTranfers] = useState([{ name: "fake transfer" }]);
+
+  useEffect(() => {
+    pollForTransfersAndUpdateIfNeeded()
+    // query for the list of transfers here
+  }, [])
+
 
   return (
     <div className="container">
